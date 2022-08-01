1ed2d6b0566ffb290a2549acdef1d48feb8e3f86
Fauve <61892539+fsetrodinomo@users.noreply.github.com>
2022-08-01 16:42:15 +0200

    wrapper , positioning spline

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import Spline from '@splinetool/react-spline';
+import Spline from "@splinetool/react-spline";
+import styled from "styled-components";
 
 export default function App() {
   return (
-    <Spline scene="https://prod.spline.design/JJxGpE4NMv20H8JA/scene.splinecode" />
+    <Wrapper>
+      <Spline
+        className="spline"
+        scene="https://prod.spline.design/JJxGpE4NMv20H8JA/scene.splinecode"
+      />
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div.attrs({ className: "df" })`
+  font-family: "Noto Serif", serif;
+  font-size: 16px;
+  color: white;
+  position: relative;
+
+  .spline {
+    position: absolute;
+    margin: 0;
+    top: 0;
+    right: 0;
+  }
+`;
