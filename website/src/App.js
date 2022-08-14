80759f003e461e8ea09573f88893a7757d014e11
Fauve <fsetrodinomo@protonmail.com>
2022-08-14 15:05:28 +0200

    start with route of page home and header

--- website/src/App.js
+++ website/src/App.js
@@ -1,14 +1,17 @@
 // import Spline from '@splinetool/react-spline'
 import './App.css';
-import { NavBar } from "./Components/NavBar.js";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router,Routes, Route} from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className='App'>
-      <NavBar />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}>
+
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ website/src/Pages/Home.js
@@ -0,0 +1,9 @@
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div classname="home"></div>
+  )
+}
+
+export default Home
