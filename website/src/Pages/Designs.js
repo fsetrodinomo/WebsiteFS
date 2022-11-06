56c0912709653015d7a91e62822e09dad3fc2bbd
Fauve <fsetrodinomo@protonmail.com>
2022-11-06 15:01:11 +0100

    finished concept design section

--- website/src/Pages/Designs.js
+++ website/src/Pages/Designs.js
@@ -13,31 +13,23 @@ const Designs = () => {
             <section className='personal' id="personal">
                 <div class="container">
                     <div class="row">
-                        <h3>
+                        <h2>
                             <p>
-                                Designs
+                                Designs                                <br></br>
                             </p>
-                        </h3>
+                        </h2>
                         <p>
-                            <i></i>
-                            Currently the only designs I created
-                            is my website and a blog design.
-                            I just want to share it to give a impression
-                            what type of designs I created until now.
-                            The blog design supposed to be a personal travel
-                            blog. Fun fact: Yes I've been to Dubai all by myself ;)
                             <br></br>
-                       </p>
-                       <cover>
-                         
+                            <i>I am clearly not a designer although I thought it
+                                would be fun to share a old design.
+                               <br></br>
+                                The only designs I created
+                            is my website and a personal travel blog design.
+                          
+                            Fun fact: Yes I've been to Dubai all by myself ;)
+                            </i>                       </p>
                        <img src={citytips1} alt="" width="500px" height="300px"/>
-                      
-                       <img src={citytips2} alt="" width="500px" height="300px"/>
-                      
-                        </cover>
-
-
-
+                        <img src={citytips2} alt="" width="500px" height="300px"/>
                     </div>
 
                 </div>
