83b51feea510469d359cae65be03f469d07fba0a
Fauve <fsetrodinomo@protonmail.com>
2022-11-07 16:06:36 +0100

    small clean up project, removed unused whitespace and unused text/code

--- website/src/App.js
+++ website/src/App.js
@@ -15,7 +15,6 @@ const App = () => {
         <Route path='/designs' element={<Designs/>}></Route>
       </Routes>
     </Router>
-    
   );
 }
 
--- website/src/Pages/Aboutme.js
+++ website/src/Pages/Aboutme.js
@@ -4,48 +4,40 @@ import './Aboutme.css'
 
 const Aboutme = () => {
     return (
-                <div className='aboutme'>
-            <Header />
+        <div className='aboutme'>
+            <Header/>
             <section className='personal' id="personal">
                 <div class="container">
-                <div className='wrapper'>
-                    <div className="words">
-                        <span>Coder.</span>
-                        <span>Stoic.</span>
-                        <span>Introvert.</span>
+                    <div className='wrapper'>
+                        <div className="words">
+                            <span>Coder.</span>
+                            <span>Stoic.</span>
+                            <span>Introvert.</span>
+                        </div>
                     </div>
-                </div>
                     <div class="row">
                         <h3>
+                            <p>
+                                I am a junior developer .
+                                Currently my main focus is React and Python.
+                                When I am bored I like to make some 3D models in Spline.
+                                The projects I like to make are applications which are focusing on connecting people.
+                                Or exploring different perspectives of existing data and  learning to share data in a way which is easily accessed
+                                for people who like a practical and/or visual approach.
+                            </p>
+                        </h3>
                         <p>
-                           
-                            I am a junior developer .
-                            Currently my main focus is React and Python.
-                            When I am bored I like to make some 3D models in Spline.
-                            The projects I like to make are applications which are focusing on connecting people.
-                            Or exploring different perspectives of existing data and  learning to share data in a way which is easily accessed
-                            for people who like a practical and/or visual approach.
-                            </p>  
-                            </h3>
-                          
-                         <p>
                             <i>
-                         Well uhhmm .. about myself?
-                            In my spare time I like to read, take a hike in nature and
-                            probarbly spend way too much time laughing at bad jokes.
-                            And playing Pokemon Sun while I could improve this website.
+                                Well uhhmm .. about myself?
+                                In my spare time I like to read, take a hike in nature and
+                                probarbly spend way too much time laughing at bad jokes.
+                                And playing Pokemon Sun while I could improve this website.
                             </i>
-                         </p>
-
-                           
-                            
+                        </p>
                     </div>
-            
-        </div>
-   </section >
-  </div >
-
-         
+                </div>
+            </section >
+        </div >
     )
 }
 
